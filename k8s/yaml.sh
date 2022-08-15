68393d08c1e1358b4e59bddf9d964566463544f6
admin <812248577@qq.com>
2022-08-15 13:22:13 +0800

    kuboard

--- k8s/yaml.sh
+++ k8s/yaml.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -i
+cd ~
 #calico网络插件
 rm -f calico.yaml
 curl https://docs.projectcalico.org/archive/v3.19/manifests/calico.yaml -O
@@ -9,32 +11,42 @@ for imageName in ${images[@]} ; do
 	crictl pull $imageName
 done
 kubectl apply -f calico.yaml&&sleep 1m
-#部署Dashboard常用扩展工具
-rm -f dashboard-recommended.yaml
-wget https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.1/aio/deploy/recommended.yaml
-mv recommended.yaml dashboard-recommended.yaml
-sed -i '42a\      nodePort: 30005' dashboard-recommended.yaml
-sed -i '39a\  type: NodePort' dashboard-recommended.yaml
-kubectl apply -f dashboard-recommended.yaml&&sleep 1m
-tee ./auth.yaml <<'EOF'
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: admin-user
-  namespace: kubernetes-dashboard
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: admin-user
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-- kind: ServiceAccount
-  name: admin-user
-  namespace: kubernetes-dashboard
-EOF
-kubectl apply -f auth.yaml&&sleep 1m
-kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
+#------------------------------------------------------------------------------------------------------
+#安装kuboard
+wget https://addons.kuboard.cn/kuboard/kuboard-v3.yaml
+kubectl apply -f kuboard-v3.yaml
+# 您也可以使用下面的指令，唯一的区别是，该指令使用华为云的镜像仓库替代 docker hub 分发 Kuboard 所需要的镜像
+# kubectl apply -f https://addons.kuboard.cn/kuboard/kuboard-v3-swr.yaml
+#	 访问端口：30080
+#    用户名： admin
+#    密码： Kuboard123
+#------------------------------------------------------------------------------------------------------
+# #安装Dashboard
+# rm -f dashboard-recommended.yaml
+# wget https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.1/aio/deploy/recommended.yaml
+# mv recommended.yaml dashboard-recommended.yaml
+# sed -i '42a\      nodePort: 30005' dashboard-recommended.yaml
+# sed -i '39a\  type: NodePort' dashboard-recommended.yaml
+# kubectl apply -f dashboard-recommended.yaml&&sleep 1m
+# tee ./auth.yaml <<'EOF'
+# apiVersion: v1
+# kind: ServiceAccount
+# metadata:
+  # name: admin-user
+  # namespace: kubernetes-dashboard
+# ---
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRoleBinding
+# metadata:
+  # name: admin-user
+# roleRef:
+  # apiGroup: rbac.authorization.k8s.io
+  # kind: ClusterRole
+  # name: cluster-admin
+# subjects:
+# - kind: ServiceAccount
+  # name: admin-user
+  # namespace: kubernetes-dashboard
+# EOF
+# kubectl apply -f auth.yaml&&sleep 1m
+# kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
