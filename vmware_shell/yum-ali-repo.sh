3f54feb415d87f4df476ae0a44158bedb401ac90
admin <812248577@qq.com>
2022-09-13 18:00:07 +0800

    repo增加epel

--- vmware_shell/yum-ali-repo.sh
+++ vmware_shell/yum-ali-repo.sh
@@ -2,5 +2,6 @@ cd /etc/yum.repos.d/
 mv CentOS-Base.repo CentOS-Base.repo.bak
 yum -y install wget
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo?spm=a2c6h.25603864.0.0.3d975969vgVeSX
+wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
 yum clean all
 yum repolist
