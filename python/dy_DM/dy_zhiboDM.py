df701edeb82bfb1eb195c62c3c33fc2ca57a5c22
admin <812248577@qq.com>
2023-01-31 17:38:14 +0800

    爬抖音直播间弹幕（打开调试功能）

--- python/dy_DM/dy_zhiboDM.py
+++ python/dy_DM/dy_zhiboDM.py
@@ -2,6 +2,7 @@ import gzip
 import binascii
 from dy_pb2 import PushFrame,Response,ChatMessage
 from websocket import WebSocketApp
+import websocket
 import json
 import re
 from urllib.parse import unquote_plus
@@ -11,7 +12,7 @@ def fetch_live_room_info(url):
     res=requests.get(
         url=url,
         headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
         },
         cookies={
             "__ac_nonce": "2023013011232740CE842751D35E26FA92"
@@ -25,14 +26,14 @@ def fetch_live_room_info(url):
     room_title = data_dict['app']['initialState']['roomStore']['roomInfo']["room"]['title']
     room_user_count = data_dict['app']['initialState']['roomStore']['roomInfo']["room"]['user_count_str']
 
-    wss_url=f"wss://webcast3-ws-web-lf.douyin.com/webcast/im/push/v2/?app_name=douyin_web&version_code=180800&webcast_sdk_version=1.3.0&update_version_code=1.3.0&compress=gzip&internal_ext=internal_src:dim|wss_push_room_id:{room_id}|wss_push_did:7194365801431533114|dim_log_id:202301301654078F75DDA92C5A60041FEB|fetch_time:1675068847837|seq:1|wss_info:0-1675068847837-0-0|wrds_kvs:InputPanelComponentSyncData-1675046737740515894_MoreLiveSyncData-1675068837641753048_HighlightContainerSyncData-2_WebcastRoomStatsMessage-1675068846997103164_WebcastRoomRankMessage-1675068847026484656&cursor=t-1675068847837_r-1_d-1_u-1_h-1&host=https://live.douyin.com&aid=6383&live_id=1&did_rule=3&debug=false&endpoint=live_pc&support_wrds=1&im_path=/webcast/im/fetch/&user_unique_id=7194365801431533114&device_platform=web&cookie_enabled=true&screen_width=1920&screen_height=1080&browser_language=zh-CN&browser_platform=Win32&browser_name=Mozilla&browser_version=5.0%20(Windows)&browser_online=true&tz_name=Asia/Shanghai&identity=audience&room_id={room_id}&heartbeatDuration=0&signature=R0BAHsL3ng2x7LqD"
+    wss_url=f"wss://webcast3-ws-web-hl.douyin.com/webcast/im/push/v2/?app_name=douyin_web&version_code=180800&webcast_sdk_version=1.3.0&update_version_code=1.3.0&compress=gzip&internal_ext=internal_src:dim|wss_push_room_id:{room_id}|wss_push_did:7194720819045713412|dim_log_id:202301311551337804232F98F5241E5599|fetch_time:1675151493644|seq:1|wss_info:0-1675151493644-0-0|wrds_kvs:InputPanelComponentSyncData-1675133124833050530_MoreLiveSyncData-1675151482619449349_WebcastRoomStatsMessage-1675151490682963987_WebcastRoomRankMessage-1675151406710703300&cursor=t-1675151493644_r-1_d-1_u-1_h-1&host=https://live.douyin.com&aid=6383&live_id=1&did_rule=3&debug=false&endpoint=live_pc&support_wrds=1&im_path=/webcast/im/fetch/&user_unique_id=7194720819045713412&device_platform=web&cookie_enabled=true&screen_width=1920&screen_height=1080&browser_language=zh-CN&browser_platform=Win32&browser_name=Mozilla&browser_version=5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/109.0.0.0%20Safari/537.36&browser_online=true&tz_name=Asia/Shanghai&identity=audience&room_id={room_id}&heartbeatDuration=0&signature=R8+hWEhaNy/Yxs17"
 
     ttwid = res.cookies.get_dict()['ttwid']
-
+    print(res.cookies)
     return room_id,room_title,room_user_count,wss_url,ttwid
 
 
-def on_open(ws,content):
+def on_open(content):
     print("on_open")
 
 def on_message(ws,content):
@@ -62,15 +63,15 @@ def on_message(ws,content):
 
 def on_error(ws,content):
     print("on_error")
-    print(content)
+    print(ws,content)
 
-def on_close(ws,content):
+def on_close(ws,content,error):
     print("on_close")
 
 
 
 def run():
-    web_url="https://live.douyin.com/80017709309"
+    web_url="https://live.douyin.com/271069055515"
     room_id,room_title,room_user_count,wss_url,ttwid=fetch_live_room_info(web_url)
     print(room_id + "\n" + room_title + "\n" + room_user_count + "\n" + wss_url + "\n" + ttwid)
     ws=WebSocketApp(
@@ -82,6 +83,7 @@ def run():
         on_error=on_error,
         on_close=on_close,
     )
+    websocket.enableTrace(True)
     ws.run_forever()
 
 if __name__ == '__main__':
