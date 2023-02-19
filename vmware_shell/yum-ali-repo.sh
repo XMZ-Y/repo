546e80f8e78d3aa52892e3df1464502eb8bad875
xmz <xmz906@dingtalk.com>
2023-02-19 22:41:51 +0800

    修改yum-ali-repo

--- vmware_shell/yum-ali-repo.sh
+++ vmware_shell/yum-ali-repo.sh
@@ -1,7 +1,5 @@
 cd /etc/yum.repos.d/
-mv CentOS-Base.repo CentOS-Base.repo.bak
-yum -y install wget
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo?spm=a2c6h.25603864.0.0.3d975969vgVeSX
-wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
-yum clean all
-yum repolist
+rm -rf ./*
+curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
+curl -o /etc/yum.repos.d/epel.repo https://mirrors.aliyun.com/repo/epel-7.repo
+yum makecache
